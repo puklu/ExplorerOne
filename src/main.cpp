a6a613b9c50bdb1e08385e4c24141de7cab2b46b
puklu <vivekb126@gmail.com>
2024-09-05 23:35:51 +0200

    add blinky

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+// src/main.cpp
+extern "C" {
+    int main();
+}
+
+#include "stm32f303xc.h"
+
+extern "C" void SystemInit() {
+    // System initialization code goes here, or leave it empty
+}
+
+void delay(volatile uint32_t count) {
+    while (count--);
+}
+
+int main() {
+    // Enable GPIOC clock
+    RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
+    
+    // Set PC9 as output
+    GPIOE->MODER |= GPIO_MODER_MODER8_0;
+    
+    while (1) {
+        // Toggle LED
+        GPIOE->ODR ^= GPIO_ODR_8;
+        delay(100000);
+    }
+}
