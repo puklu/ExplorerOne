fc6bfcd3679b27b932cc70ad8f207e9b13d39caa
puklu <vivekb126@gmail.com>
2024-09-12 22:19:07 +0200

    formatting: add clang-format back as a build dependency
    Added clang-format as a dependency so that it automatically runs
    during build and formats the files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,32 @@
 // src/main.cpp
-extern "C" {
+extern "C"
+{
     int main();
 }
 
 #include "stm32f303xc.h"
 
-extern "C" void SystemInit() {
+extern "C" void SystemInit()
+{
     // System initialization code goes here, or leave it empty
 }
 
-void delay(volatile uint32_t count) {
-    while (count--);
+void delay(volatile uint32_t count)
+{
+    while (count--)
+        ;
 }
 
-int main() {
+int main()
+{
     // Enable GPIOC clock
     RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
-    
+
     // Set PC9 as output
     GPIOE->MODER |= GPIO_MODER_MODER8_0;
-    
-    while (1) {
+
+    while (1)
+    {
         // Toggle LED
         GPIOE->ODR ^= GPIO_ODR_8;
         delay(100000);
