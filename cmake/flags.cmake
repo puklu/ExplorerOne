0a7fae5842ba7c2db6d8fd5a1577c093cd226f1c
puklu <vivekb126@gmail.com>
2024-09-15 13:34:45 +0000

    build-system: Clean up build system by making it modular
    Split the root level CMakeLists into multiple level by adding
    CMakeLists for each module.

---
+++ cmake/flags.cmake
@@ -0,0 +1,16 @@
+# Set the cross-compiler
+set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
+set(CMAKE_CXX_COMPILER "arm-none-eabi-g++")
+set(CMAKE_ASM_COMPILER "arm-none-eabi-gcc")
+set(CMAKE_OBJCOPY "arm-none-eabi-objcopy")
+
+# Set compiler flags
+set(CPU_FLAGS "-mcpu=cortex-m4 -mthumb")
+set(COMMON_FLAGS "-g3 -O0 -Wall -Werror -Wextra -Wshadow -fdata-sections -ffunction-sections")
+set(CMAKE_C_FLAGS "${CPU_FLAGS} ${COMMON_FLAGS}")
+set(CMAKE_CXX_FLAGS "${CPU_FLAGS} ${COMMON_FLAGS} -fno-exceptions -fno-rtti")
+set(CMAKE_ASM_FLAGS "${CPU_FLAGS} ${COMMON_FLAGS}")
+
+# Linker flags
+set(LINKER_SCRIPT "${CMAKE_SOURCE_DIR}/STM32F303VCTX_FLASH.ld")
+set(CMAKE_EXE_LINKER_FLAGS "${CPU_FLAGS} -T ${LINKER_SCRIPT} -Wl,--gc-sections")
---
+++ cmake/formatting.cmake
@@ -0,0 +1,30 @@
+find_program(CLANG_FORMAT_EXECUTABLE NAMES clang-format) 
+
+# Specify files to run clang-format on
+set(FORMAT_DIRECTORIES 
+    src/app/ 
+    src/drivers/ 
+    src/common/ 
+    src/test)
+
+file(GLOB_RECURSE FORMAT_FILES
+    ${FORMAT_DIRECTORIES}/*.hpp
+    ${FORMAT_DIRECTORIES}/*.cpp
+)
+
+# Function to run clang-format
+add_custom_target(
+    clang-format
+    COMMAND ${CLANG_FORMAT_EXECUTABLE}
+    -i ${FORMAT_FILES}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    COMMENT "Running clang-format..."
+)
+
+# Add a target to check code formatting (no -i flag)
+add_custom_target(
+    clang-format-check
+    COMMAND ${CLANG_FORMAT_EXECUTABLE} --dry-run -Werror ${SOURCES}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    COMMENT "Checking code format..."
+)
