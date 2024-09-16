7f9023c06c69334a3575c2d4a310535834204718
puklu <vivekb126@gmail.com>
2024-09-16 19:27:36 +0000

    fix format

--- src/drivers/io/io.hpp
+++ src/drivers/io/io.hpp
@@ -9,9 +9,9 @@
 
 /**
  * @namespace IO
- * @brief All IO related variables, constants and functions should fall 
+ * @brief All IO related variables, constants and functions should fall
  * under this namespace
-*/
+ */
 namespace IO
 {
 
@@ -23,30 +23,32 @@ namespace IO
  * @enum
  * @brief The purpose of this enum is to extract port number and pin number
  * from an enum. There are 6(ports) times 16(pin in each port) numbers in this
- * enum. 
+ * enum.
  * @example The number represented by PB1 is 17, which is 0001 0001
  *          Since there are 16 pins per port, we need 4 bits. The last
- *          4 bits can be used to extract pin number. And there are 6 
+ *          4 bits can be used to extract pin number. And there are 6
  *          ports so the next 3 bits can be used to extract port.
- *          
+ *
  *          PB1 = 0001 0001
- *          pin = PB1 & 0x0F = 0000 0001, which is 1 (pin indexing starts from 0)
- *                here 0X0F is PIN_MASK
- * 
- *          port = (PB1 & 0X70) >> 4 = 0000 0001, which is 1 (port indexing starts from 0)
- *                 here 0x70 is PORT_MASK, and 4 is PORT_OFFSET
- *  
-*/
+ *          pin = PB1 & 0x0F = 0000 0001, which is 1 (pin indexing starts from
+ * 0) here 0X0F is PIN_MASK
+ *
+ *          port = (PB1 & 0X70) >> 4 = 0000 0001, which is 1 (port indexing
+ * starts from 0) here 0x70 is PORT_MASK, and 4 is PORT_OFFSET
+ *
+ */
 
-typedef enum : uint8_t {
+typedef enum : uint8_t
+{
+    // clang-format off
     PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7, PA8, PA9, PA10, PA11, PA12, PA13, PA14, PA15,
     PB0, PB1, PB2, PB3, PB4, PB5, PB6, PB7, PB8, PB9, PB10, PB11, PB12, PB13, PB14, PB15,
     PC0, PC1, PC2, PC3, PC4, PC5, PC6, PC7, PC8, PC9, PC10, PC11, PC12, PC13, PC14, PC15,
     PD0, PD1, PD2, PD3, PD4, PD5, PD6, PD7, PD8, PD9, PD10, PD11, PD12, PD13, PD14, PD15,
     PE0, PE1, PE2, PE3, PE4, PE5, PE6, PE7, PE8, PE9, PE10, PE11, PE12, PE13, PE14, PE15,
     PF0, PF1, PF2, PF3, PF4, PF5, PF6, PF7, PF8, PF9, PF10, PF11, PF12, PF13, PF14, PF15,
+    // clang-format on
 } io_generic_port;
-
 
 struct port_a
 {
