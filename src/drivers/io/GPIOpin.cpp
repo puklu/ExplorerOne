13ac90a36a4d4ebd4dbb51910821a5d3340cf6e6
puklu <vivekb126@gmail.com>
2024-09-23 20:17:43 +0000

    mcu init: move SystemInit() to a separate file

--- src/drivers/io/GPIOpin.cpp
+++ src/drivers/io/GPIOpin.cpp
@@ -2,12 +2,6 @@
 
 #include "registerArrays.hpp"
 
-void SystemInit()
-{
-    // TODO: Move to MCU_init() or something
-    // System initialization code goes here, or leave it empty
-}
-
 namespace IO
 {
 
---
+++ src/drivers/mcu/mcuInit.cpp
@@ -0,0 +1,7 @@
+#include "mcuInit.hpp"
+
+void SystemInit()
+{
+    // TODO: Move to MCU_init() or something
+    // System initialization code goes here, or leave it empty
+}
