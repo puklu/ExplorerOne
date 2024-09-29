1a31c0309d1ab03a364ce8010e3af24c96f48a2f
puklu <vivekb126@gmail.com>
2024-09-29 15:49:26 +0000

    build system: remove dependency of common on drivers

--- src/common/assertHandler.cpp
+++ src/common/assertHandler.cpp
@@ -1,25 +1,9 @@
 #include "assertHandler.hpp"
 
 #include "delay.hpp"
-#include "io/GPIOpin.hpp"
-#include "io/enums.hpp"
 
 void assertHandler()
 {
     // TODO: Improve me
     SOFTWARE_BREAKPOINT
-
-    IO::GPIOpin pin = IO::GPIOpin(IO::ePin::IO_ASSERT_ENCOUNTERED_LED);
-
-    pin.SetOutputType(IO::eOutputType::IO_OUTPUT_TYPE_PUSH_PULL);
-    pin.SetResistor(IO::ePupdResistor::IO_RESISTOR_PULL_DOWN);
-    pin.SetMode(IO::eMode::IO_MODE_OUTPUT);
-
-    while (1)
-    {
-        pin.WriteOutputValue(IO::eValue::IO_VALUE_HIGH);
-        delay(2500000);
-        pin.WriteOutputValue(IO::eValue::IO_VALUE_LOW);
-        delay(2500000);
-    }
 }
