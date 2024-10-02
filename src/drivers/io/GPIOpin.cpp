e0985de4bb0ce589ea26c2b75b59bdb6a1d7b08b
puklu <vivekb126@gmail.com>
2024-10-02 19:52:53 +0000

    interrupts: crude starting for interrupt interface
    Also some minor changes in GPIOpin class

--- src/drivers/io/GPIOpin.cpp
+++ src/drivers/io/GPIOpin.cpp
@@ -1,21 +1,30 @@
 #include "GPIOpin.hpp"
 
 #include "registerArrays.hpp"
+#include "assertHandler.hpp"
 
 namespace IO
 {
 
 GPIOpin::GPIOpin(ePin pin_name) : mPinName(pin_name)
 {
-    SetPortNumber();
-    SetPinNumber();
+
     Enable();
     mIsInitialized = true;
+}
+
+void GPIOpin::Enable()
+{
+    // TODO: add assert
+    SetPortNumber();
+    SetPinNumber();
+    mpRCC->AHBENR |= aPortEnableRegisters[mPortNumber];
 }
 
 void GPIOpin::SetPortNumber()
 {
     mPortNumber = (mPinName & IO_PORT_MASK) >> IO_PORT_OFFSET;
+    mpPort = aPorts[mPortNumber];
 }
 
 void GPIOpin::SetPinNumber()
@@ -23,35 +32,29 @@ void GPIOpin::SetPinNumber()
     mPinNumber = mPinName & IO_PIN_MASK;
 }
 
-void GPIOpin::Enable() const
-{
-    // TODO: add assert
-    RCC->AHBENR |= aPortEnableRegisters[mPortNumber];
-}
 
 void GPIOpin::SetMode(IO::eMode mode)
 {
     // TODO: add assert
-    volatile GPIO_TypeDef *pPort = aPorts[mPortNumber];
-    mMode                        = mode;
+    mMode = mode;
 
     switch (mode)
     {
         case IO::eMode::IO_MODE_INPUT:
-            pPort->MODER &= ~aModeRegisterBits[2 * mPinNumber + 1];
-            pPort->MODER &= ~aModeRegisterBits[2 * mPinNumber];
+            mpPort->MODER &= ~aModeRegisterBits[2 * mPinNumber + 1];
+            mpPort->MODER &= ~aModeRegisterBits[2 * mPinNumber];
             break;
         case IO::eMode::IO_MODE_OUTPUT:
-            pPort->MODER &= ~aModeRegisterBits[2 * mPinNumber + 1];
-            pPort->MODER |= aModeRegisterBits[2 * mPinNumber];
+            mpPort->MODER &= ~aModeRegisterBits[2 * mPinNumber + 1];
+            mpPort->MODER |= aModeRegisterBits[2 * mPinNumber];
             break;
         case IO::eMode::IO_MODE_ALT_FUNCTION:
-            pPort->MODER |= aModeRegisterBits[2 * mPinNumber + 1];
-            pPort->MODER &= ~aModeRegisterBits[2 * mPinNumber];
+            mpPort->MODER |= aModeRegisterBits[2 * mPinNumber + 1];
+            mpPort->MODER &= ~aModeRegisterBits[2 * mPinNumber];
             break;
         case IO::eMode::IO_MODE_ANALOG:
-            pPort->MODER |= aModeRegisterBits[2 * mPinNumber];
-            pPort->MODER |= aModeRegisterBits[2 * mPinNumber + 1];
+            mpPort->MODER |= aModeRegisterBits[2 * mPinNumber];
+            mpPort->MODER |= aModeRegisterBits[2 * mPinNumber + 1];
             break;
         default:
             break;
@@ -61,16 +64,15 @@ void GPIOpin::SetMode(IO::eMode mode)
 void GPIOpin::SetOutputType(eOutputType outType)
 {
     // TODO: add assert
-    volatile GPIO_TypeDef *pPort = aPorts[mPortNumber];
     mOutputType                  = outType;
 
     switch (outType)
     {
         case IO::eOutputType::IO_OUTPUT_TYPE_PUSH_PULL:
-            pPort->OTYPER &= ~aOutputTypeRegisterBits[mPinNumber];
+            mpPort->OTYPER &= ~aOutputTypeRegisterBits[mPinNumber];
             break;
         case IO::eOutputType::IO_OUTPUT_TYPE_OPEN_DRAIN:
-            pPort->OTYPER |= aOutputTypeRegisterBits[mPinNumber];
+            mpPort->OTYPER |= aOutputTypeRegisterBits[mPinNumber];
             break;
         default:
             break;
@@ -80,22 +82,21 @@ void GPIOpin::SetOutputType(eOutputType outType)
 void GPIOpin::SetOutputSpeed(eOutputSpeed outSpeed)
 {
     // TODO: add assert
-    volatile GPIO_TypeDef *pPort = aPorts[mPortNumber];
     mOutputSpeed                 = outSpeed;
 
     switch (outSpeed)
     {
         case IO::eOutputSpeed::IO_OUTPUT_LOW_SPEED:
-            pPort->MODER &= ~aOutputSpeedRegisterBits[2 * mPinNumber + 1];
-            pPort->MODER &= ~aOutputSpeedRegisterBits[2 * mPinNumber];
+            mpPort->MODER &= ~aOutputSpeedRegisterBits[2 * mPinNumber + 1];
+            mpPort->MODER &= ~aOutputSpeedRegisterBits[2 * mPinNumber];
             break;
         case IO::eOutputSpeed::IO_OUTPUT_MEDIUM_SPEED:
-            pPort->MODER &= ~aOutputSpeedRegisterBits[2 * mPinNumber + 1];
-            pPort->MODER |= aOutputSpeedRegisterBits[2 * mPinNumber];
+            mpPort->MODER &= ~aOutputSpeedRegisterBits[2 * mPinNumber + 1];
+            mpPort->MODER |= aOutputSpeedRegisterBits[2 * mPinNumber];
             break;
         case IO::eOutputSpeed::IO_OUTPUT_HIGH_SPEED:
-            pPort->MODER |= aOutputSpeedRegisterBits[2 * mPinNumber + 1];
-            pPort->MODER |= aOutputSpeedRegisterBits[2 * mPinNumber];
+            mpPort->MODER |= aOutputSpeedRegisterBits[2 * mPinNumber + 1];
+            mpPort->MODER |= aOutputSpeedRegisterBits[2 * mPinNumber];
             break;
         default:
             break;
@@ -105,26 +106,25 @@ void GPIOpin::SetOutputSpeed(eOutputSpeed outSpeed)
 void GPIOpin::SetResistor(IO::ePupdResistor updown)
 {
     // TODO: add assert
-    volatile GPIO_TypeDef *pPort = aPorts[mPortNumber];
     mPupdResistor                = updown;
 
     switch (updown)
     {
         case IO::ePupdResistor::IO_RESISTOR_NO_PUPD:
-            pPort->PUPDR &= ~aPullupPulldownRegisterBits[2 * mPinNumber + 1];
-            pPort->PUPDR &= ~aPullupPulldownRegisterBits[2 * mPinNumber];
+            mpPort->PUPDR &= ~aPullupPulldownRegisterBits[2 * mPinNumber + 1];
+            mpPort->PUPDR &= ~aPullupPulldownRegisterBits[2 * mPinNumber];
             break;
         case IO::ePupdResistor::IO_RESISTOR_PULL_UP:
-            pPort->PUPDR &= ~aPullupPulldownRegisterBits[2 * mPinNumber + 1];
-            pPort->PUPDR |= aPullupPulldownRegisterBits[2 * mPinNumber];
+            mpPort->PUPDR &= ~aPullupPulldownRegisterBits[2 * mPinNumber + 1];
+            mpPort->PUPDR |= aPullupPulldownRegisterBits[2 * mPinNumber];
             break;
         case IO::ePupdResistor::IO_RESISTOR_PULL_DOWN:
-            pPort->PUPDR |= aPullupPulldownRegisterBits[2 * mPinNumber + 1];
-            pPort->PUPDR &= ~aPullupPulldownRegisterBits[2 * mPinNumber];
+            mpPort->PUPDR |= aPullupPulldownRegisterBits[2 * mPinNumber + 1];
+            mpPort->PUPDR &= ~aPullupPulldownRegisterBits[2 * mPinNumber];
             break;
         case IO::ePupdResistor::IO_RESISTOR_RESERVED:
-            pPort->PUPDR |= aPullupPulldownRegisterBits[2 * mPinNumber];
-            pPort->PUPDR |= aPullupPulldownRegisterBits[2 * mPinNumber + 1];
+            mpPort->PUPDR |= aPullupPulldownRegisterBits[2 * mPinNumber];
+            mpPort->PUPDR |= aPullupPulldownRegisterBits[2 * mPinNumber + 1];
             break;
         default:
             break;
@@ -134,10 +134,8 @@ void GPIOpin::SetResistor(IO::ePupdResistor updown)
 IO::eValue GPIOpin::ReadInputValue()
 {
     // TODO: add assert
-    volatile GPIO_TypeDef *pPort = aPorts[mPortNumber];
-
     mValueAtPin = IO::eValue(
-        (pPort->IDR & aInputDataRegisterBits[mPinNumber]) >> mPinNumber);
+        (mpPort->IDR & aInputDataRegisterBits[mPinNumber]) >> mPinNumber);
 
     return mValueAtPin;
 }
@@ -145,10 +143,8 @@ IO::eValue GPIOpin::ReadInputValue()
 IO::eValue GPIOpin::ReadOutputValue()
 {
     // TODO: add assert
-    volatile GPIO_TypeDef *pPort = aPorts[mPortNumber];
-
     mValueAtPin = IO::eValue(
-        (pPort->ODR & aOutputDataRegisterBits[mPinNumber]) >> mPinNumber);
+        (mpPort->ODR & aOutputDataRegisterBits[mPinNumber]) >> mPinNumber);
 
     return mValueAtPin;
 }
@@ -156,22 +152,56 @@ IO::eValue GPIOpin::ReadOutputValue()
 void GPIOpin::WriteOutputValue(IO::eValue value)
 {
     // TODO: add assert
-    volatile GPIO_TypeDef *pPort = aPorts[mPortNumber];
     mValueAtPin                  = value;
 
     switch (value)
     {
         case IO_VALUE_LOW:
-            pPort->ODR &= ~aOutputDataRegisterBits[mPinNumber];
+            mpPort->ODR &= ~aOutputDataRegisterBits[mPinNumber];
             break;
 
         case IO_VALUE_HIGH:
-            pPort->ODR |= aOutputDataRegisterBits[mPinNumber];
+            mpPort->ODR |= aOutputDataRegisterBits[mPinNumber];
             break;
 
         default:
             break;
     }
+}
+
+void GPIOpin::EnableInterrupt(){
+
+    // TODO: add assert
+
+    // Enable system configuration
+    mpRCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+ 
+    // Map the GPIO pin to EXTI line
+    mpSystemConfigController->EXTICR[mPinNumber / 4] |= aSyscfgExtiRegisterBits[mPinNumber];
+    mpSystemConfigController->EXTICR[mPinNumber / 4] &= (mPortNumber << (4* (mPinNumber % 4)));
+ 
+}
+
+void GPIOpin::SelectInterruptTrigger(IO::eTriggerEdge edge){
+    // TODO: add assert
+    switch (edge)
+    {
+    case IO::eTriggerEdge::IO_INTERRUPT_TRIGGER_RISING_EDGE:
+        mpInterruptController->RTSR |= (1 << mPinNumber);
+        break;
+
+    case IO::eTriggerEdge::IO_INTERRUPT_TRIGGER_FALLING_EDGE:
+        mpInterruptController->FTSR |= (1 << mPinNumber);
+        break;
+
+    case IO::eTriggerEdge::IO_INTERRUPT_TRIGGER_BOTH_EDGE:
+        mpInterruptController->RTSR |= (1 << mPinNumber);
+        mpInterruptController->FTSR |= (1 << mPinNumber);
+        break;      
+    default:
+        ASSERT(0);
+    }
+    
 }
 
 }  // namespace IO
--- tests/main.cpp
+++ tests/main.cpp
@@ -3,6 +3,6 @@
 
 int main()
 {
-    IO::GPIOpin pin = IO::GPIOpin(IO::ePin::IO_TEST_LED_LD3);
+    IO::GPIOpin pin = IO::GPIOpin(IO::ePin::IO_TEST_LED_LD4);
     blinkyTestFunction(pin);
 }
