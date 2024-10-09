fd5a4ee4a1404c8d23eec83a7bf8a2fd6154e0ae
puklu <vivekb126@gmail.com>
2024-10-09 21:34:32 +0000

    interrupts: add supporting code for interrupts

--- src/common/defines.hpp
+++ src/common/defines.hpp
@@ -1,1 +1,9 @@
+#pragma once
 
+#define IO_PIN_MASK           (0x0F)
+#define IO_PORT_OFFSET        (4u)
+#define IO_PORT_MASK          (0x70)
+#define IO_PORT_COUNT         (6u)
+#define IO_PIN_COUNT_PER_PORT (16u)
+#define IO_BITS_PER_REGISTER  (32u)
+#define IO_EXTI_LINES_COUNT   (36u)
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,17 @@
 #include "common/assertHandler.hpp"
 #include "common/delay.hpp"
 #include "drivers/io/GPIOpin.hpp"
+#include "drivers/leds/leds.hpp"
+#include "drivers/io/ISRs.cpp"
+
 
 int main()
 {
-    ASSERT(0);
+    IO::GPIOpin pin = IO::GPIOpin(IO::ePin::IO_UNUSED_B15);
+    pin.SetMode(IO::eMode::IO_MODE_INPUT);
+    pin.SetResistor(IO::ePupdResistor::IO_RESISTOR_PULL_DOWN);
+    pin.EnableInterrupt(InterruptLed);
+    pin.SelectInterruptTrigger(IO::eTriggerEdge::IO_INTERRUPT_TRIGGER_RISING_EDGE);
+
+    while(1){}
 }
