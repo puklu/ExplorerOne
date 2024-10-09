b500cd79271f38a9a2bf36783a9f630fd3cb229b
puklu <vivekb126@gmail.com>
2024-10-09 21:32:28 +0000

    tests: add test for interrupts

---
+++ tests/interrupt.cpp
@@ -0,0 +1,15 @@
+#include "interrupt.hpp"
+
+#include "common/delay.hpp"
+#include "drivers/io/ISRs.cpp"
+#include "drivers/leds/leds.hpp"
+
+void interruptTestFunction()
+{
+    IO::GPIOpin pin = IO::GPIOpin(IO::ePin::IO_UNUSED_B15);
+    pin.SetMode(IO::eMode::IO_MODE_INPUT);
+    pin.SetResistor(IO::ePupdResistor::IO_RESISTOR_PULL_DOWN);
+    pin.EnableInterrupt(InterruptLed);
+    pin.SelectInterruptTrigger(
+        IO::eTriggerEdge::IO_INTERRUPT_TRIGGER_RISING_EDGE);
+}
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,11 @@
 #include "blinky.hpp"
 #include "drivers/io/GPIOpin.hpp"
+#include "interrupt.hpp"
 
 int main()
 {
-    IO::GPIOpin pin = IO::GPIOpin(IO::ePin::IO_TEST_LED_LD4);
-    blinkyTestFunction(pin);
+    // IO::GPIOpin pin = IO::GPIOpin(IO::ePin::IO_TEST_LED_LD4);
+    // blinkyTestFunction(pin);
+
+    interruptTestFunction();
 }
