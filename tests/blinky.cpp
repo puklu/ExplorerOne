f4a71b3461cfad7d60b0f4398e5a642624c39edb
puklu <vivekb126@gmail.com>
2024-10-10 21:07:08 +0000

    io: change to using a struct to initilise pin
    Added a struct that the user has to use to define
    all the initilizing values for a pin, which is then
    used in the GPIOpin contructor do all in one step.

--- tests/blinky.cpp
+++ tests/blinky.cpp
@@ -2,15 +2,11 @@
 
 void blinkyTestFunction(IO::GPIOpin &pin)
 {
-    pin.SetOutputType(IO::eOutputType::IO_OUTPUT_TYPE_PUSH_PULL);
-    pin.SetResistor(IO::ePupdResistor::IO_RESISTOR_PULL_DOWN);
-    pin.SetMode(IO::eMode::IO_MODE_OUTPUT);
-
     while (1)
     {
         pin.WriteOutputValue(IO::eValue::IO_VALUE_HIGH);
-        delay(100000);
+        delay(250000);
         pin.WriteOutputValue(IO::eValue::IO_VALUE_LOW);
-        delay(100000);
+        delay(250000);
     }
 }
