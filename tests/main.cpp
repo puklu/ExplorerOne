2c2ca8c6494cce1caea1e6f7ccb719e319304412
puklu <vivekb126@gmail.com>
2024-10-10 21:58:19 +0000

    todo fix: test for interrupts is not working. fix it.

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,16 +4,16 @@
 
 int main()
 {
-    IO::GpioPinInitStruct pinInit = {
-        .pin_name      = IO::ePin::IO_TEST_LED_LD4_BLUE,
-        .mode          = IO::eMode::IO_MODE_OUTPUT,
-        .output_type   = IO::eOutputType::IO_OUTPUT_TYPE_PUSH_PULL,
-        .pupd_resistor = IO::ePupdResistor::IO_RESISTOR_PULL_DOWN,
-    };
+    // IO::GpioPinInitStruct pinInit = {
+    //     .pin_name      = IO::ePin::IO_TEST_LED_LD4_BLUE,
+    //     .mode          = IO::eMode::IO_MODE_OUTPUT,
+    //     .output_type   = IO::eOutputType::IO_OUTPUT_TYPE_PUSH_PULL,
+    //     .pupd_resistor = IO::ePupdResistor::IO_RESISTOR_PULL_DOWN,
+    // };
 
-    IO::GPIOpin pin(pinInit);
+    // IO::GPIOpin pin(pinInit);
 
-    blinkyTestFunction(pin);
+    // blinkyTestFunction(pin);
 
-    // interruptTestFunction();
+    interruptTestFunction();
 }
