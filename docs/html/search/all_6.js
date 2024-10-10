fb0610f5b2d80c3f57ecfcef3881b284557f0496
puklu <vivekb126@gmail.com>
2024-10-10 22:03:48 +0000

    documentation: generate documentation

--- docs/html/search/all_6.js
+++ docs/html/search/all_6.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['gdb_20to_20flash_20the_20device_0',['Use GDB to Flash the Device',['../md_docs_2how__to_2work__with__board.html#autotoc_md6',1,'']]],
   ['getinterruptcallback_1',['getInterruptCallback',['../classIO_1_1GPIOpin.html#a7c4199db255ff2cd61996f143adfc8ef',1,'IO::GPIOpin']]],
-  ['gpiopin_2',['GPIOpin',['../classIO_1_1GPIOpin.html',1,'IO']]]
+  ['gpiopin_2',['GPIOpin',['../classIO_1_1GPIOpin.html',1,'IO']]],
+  ['gpiopininitstruct_3',['GpioPinInitStruct',['../structIO_1_1GpioPinInitStruct.html',1,'IO']]]
 ];
--- docs/html/search/classes_0.js
+++ docs/html/search/classes_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['gpiopin_0',['GPIOpin',['../classIO_1_1GPIOpin.html',1,'IO']]]
+  ['gpiopin_0',['GPIOpin',['../classIO_1_1GPIOpin.html',1,'IO']]],
+  ['gpiopininitstruct_1',['GpioPinInitStruct',['../structIO_1_1GpioPinInitStruct.html',1,'IO']]]
 ];
