85516cc827d0b87c0a6ba7c386a7c07824214548
puklu <vivekb126@gmail.com>
2024-10-11 18:58:49 +0000

    improvement: moved interuppt entry point to tests.

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,7 @@
 
 int main()
 {
-
-    IO::GpioPinInitStruct pinInit = {
-        .pin_name = IO::ePin::IO_UNUSED_D0,
-        .mode = IO::eMode::IO_MODE_INPUT,
-        .pupd_resistor = IO::ePupdResistor::IO_RESISTOR_PULL_DOWN,
-    };
-
-    IO::GPIOpin pin(pinInit);
-
-    pin.EnableInterrupt(InterruptLed);
-    pin.SelectInterruptTrigger(IO::eTriggerEdge::IO_INTERRUPT_TRIGGER_RISING_EDGE);
-
-    while(1){}
+    while (1)
+    {
+    }
 }
