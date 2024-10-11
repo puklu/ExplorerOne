6367ae1d19db0e1d9e0ddbf9bdc415ad2bdeba07
puklu <vivekb126@gmail.com>
2024-10-11 18:57:33 +0000

    tests: fix tests for interrupt.
    Each test has its main function in its own cpp now so that
    you don't have to manually comment/uncomment main to run specific
    tests.

--- tests/blinky.cpp
+++ tests/blinky.cpp
@@ -10,3 +10,17 @@ void blinkyTestFunction(IO::GPIOpin &pin)
         delay(250000);
     }
 }
+
+int main()
+{
+    IO::GpioPinInitStruct pinInit = {
+        .pin_name      = IO::ePin::IO_TEST_LED_LD4_BLUE,
+        .mode          = IO::eMode::IO_MODE_OUTPUT,
+        .output_type   = IO::eOutputType::IO_OUTPUT_TYPE_PUSH_PULL,
+        .pupd_resistor = IO::ePupdResistor::IO_RESISTOR_PULL_DOWN,
+    };
+
+    IO::GPIOpin pin(pinInit);
+
+    blinkyTestFunction(pin);
+}
