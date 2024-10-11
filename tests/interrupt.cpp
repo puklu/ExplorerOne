d3a2d245cfe6d1603ff19c90a6cc26f2e0d04549
puklu <vivekb126@gmail.com>
2024-10-11 18:57:33 +0000

    tests: fix tests for interrupt.
    Each test has its main function in its own cpp now so that
    you don't have to manually comment/uncomment main to run specific
    tests.

--- tests/interrupt.cpp
+++ tests/interrupt.cpp
@@ -4,10 +4,10 @@
 #include "drivers/io/IrqHandlers.cpp"
 #include "drivers/leds/leds.hpp"
 
-void interruptTestFunction()
+int main()
 {
     IO::GpioPinInitStruct pinInit = {
-        .pin_name      = IO::ePin::IO_UNUSED_B15,
+        .pin_name      = IO::ePin::IO_UNUSED_D0,
         .mode          = IO::eMode::IO_MODE_INPUT,
         .pupd_resistor = IO::ePupdResistor::IO_RESISTOR_PULL_DOWN,
     };
@@ -17,4 +17,8 @@ void interruptTestFunction()
     pin.EnableInterrupt(InterruptLed);
     pin.SelectInterruptTrigger(
         IO::eTriggerEdge::IO_INTERRUPT_TRIGGER_RISING_EDGE);
+
+    while (1)
+    {
+    }
 }
