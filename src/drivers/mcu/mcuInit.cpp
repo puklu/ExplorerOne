291aaaf0ed962e9d8b8957d248d6b009234e7010
puklu <vivekb126@gmail.com>
2024-10-17 19:08:38 +0000

    mcuinit: add disabling watchog. Only commented out for now.

--- src/drivers/mcu/mcuInit.cpp
+++ src/drivers/mcu/mcuInit.cpp
@@ -1,6 +1,12 @@
 #include "mcuInit.hpp"
+#include "stm32f303xc.h"
 
 void SystemInit()
 {
     // TODO: Handle watchdog here
+    // WWDG->CR &= ~ (1<<7);  //Disable watchdog
+
+    // DBGMCU->APB1FZ |= DBGMCU_APB1_FZ_DBG_IWDG_STOP;  // Freeze IWDG in debug mode
+    // DBGMCU->APB1FZ |= DBGMCU_APB1_FZ_DBG_WWDG_STOP;  // Freeze WWDG in debug mode
+
 }
