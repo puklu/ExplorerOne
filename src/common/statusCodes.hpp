4ea3efde08499534bb10278985773c526bfce022
puklu <vivekb126@gmail.com>
2024-10-22 20:15:14 +0000

    comments: add doxygen comments

--- src/common/statusCodes.hpp
+++ src/common/statusCodes.hpp
@@ -1,7 +1,19 @@
-#pragma once
-#include <cstdint>  // for uintx_t
+/**
+ * @file statusCodes.hpp
+ * @brief Defines status codes.
+ */
 
-enum class eRingBufferStatus : uintptr_t
+#pragma once
+#include <cstdint>  // for uint8_t
+
+/**
+ * @enum eRingBufferStatus
+ * @brief Enumerates possible status codes for RingBuffer operations.
+ *
+ * The eRingBufferStatus enum represents the status of operations performed
+ * by the RingBuffer class, such as adding, removing, or peeking at data.
+ */
+enum class eRingBufferStatus : uint8_t
 {
     RING_BUFFER_STATUS_SUCCESS,
     RING_BUFFER_STATUS_EMPTY,
