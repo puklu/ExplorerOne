5a5551389ead7b90986c655c184e7555873fde58
puklu <vivekb126@gmail.com>
2024-10-22 20:26:18 +0000

    comments: add doxygen comments

--- src/drivers/usart/usart.hpp
+++ src/drivers/usart/usart.hpp
@@ -1,3 +1,11 @@
+/**
+ * @file usart.hpp
+ * @brief Provides the USART namespace for configuring and using USART communication.
+ *
+ * This file contains the declaration of functions and structures for configuring USART
+ * parameters, initializing the peripheral, and sending/receiving data using the USART module.
+ */
+
 #pragma once
 
 #include "stm32f303xc.h"
@@ -9,6 +17,13 @@
 namespace USART
 {
 
+/**
+ * @struct UsartInitStruct
+ * @brief Represents the initialization parameters for the USART peripheral.
+ *
+ * This structure contains configuration settings such as baud rate, word length, 
+ * parity control, interrupts, and transmission/reception settings for the USART module.
+ */
 typedef struct
 {
     IO::GPIOpin *pin;
@@ -23,17 +38,50 @@ typedef struct
     eTxEnable tx_enable = eTxEnable::USART_TX_ENABLE;
     eRxEnable rx_enable = eRxEnable::USART_RX_ENABLE;
     eUsartEnable usart_enable = eUsartEnable::USART_ENABLE;
-    eBaudRate baud_rate = eBaudRate::USART_BAUD_RATE_9600;
+    eBaudRate baud_rate = eBaudRate::USART_BAUD_RATE_115200;
 } UsartInitStruct;
 
+
+/**
+ * @brief Pointer to the USART peripheral instance.
+ */
 extern USART_TypeDef* Usart;
 
+
+/**
+ * @brief Initializes the USART peripheral with the given configuration.
+ * @param usart_init_struct The initialization structure with USART settings.
+ */
 void UsartInit(UsartInitStruct const &usart_init_struct);
 
+
+/**
+ * @brief Enables the clock for the USART peripheral.
+ */
 void EnableClock();
 
+
+/**
+ * @brief Configures the GPIO pin for USART communication with the specified alternate function.
+ * @param pin The GPIO pin to be used for USART.
+ * @param alternate_function The alternate function setting for the GPIO pin.
+ */
 void SelectUsart(IO::GPIOpin *pin, IO::eAlternateFunction alternate_function);
 
+
+/**
+ * @brief Configures the USART control register with the specified parameters.
+ * @param word_length The word length setting.
+ * @param oversampling_mode The oversampling mode.
+ * @param parity_control The parity control enable setting.
+ * @param parity_selection The parity selection.
+ * @param tx_interrupt The transmit interrupt enable setting.
+ * @param tx_complete_interrupt The transmit complete interrupt enable setting.
+ * @param rx_not_empty_interrupt The receive not empty interrupt enable setting.
+ * @param tx_enable The transmitter enable setting.
+ * @param rx_enable The receiver enable setting.
+ * @param usart_enable The USART enable setting.
+ */
 void SetControlRegister(eWordLength &word_length,
     eOverSamplingMode &oversampling_mode,
     eParityControlEnable &parity_control,
@@ -45,6 +93,12 @@ void SetControlRegister(eWordLength &word_length,
     eRxEnable &rx_enable,
     eUsartEnable &usart_enable);
 
+
+/**
+ * @brief Sets the baud rate for USART communication.
+ * @param baud_rate The desired baud rate setting.
+ * @param oversampling_mode The oversampling mode to be used.
+ */
 void SetBaudRate(const eBaudRate &baud_rate, const eOverSamplingMode &oversampling_mode);
 
 // void SetGuardTimeAndPrescaler(UsartInitStruct &usart_init_struct);
@@ -53,12 +107,33 @@ void SetBaudRate(const eBaudRate &baud_rate, const eOverSamplingMode &oversampli
 
 // void SetRequestRegister(UsartInitStruct &usart_init_struct);
 
+
+/**
+ * @brief Retrieves the interrupt and status register value based on the specified flag.
+ * @param flag The flag indicating which status to check.
+ * @return The value of the interrupt and status register.
+ */
 uint32_t GetInterruptAndStatusRegister(eIsrFlags &flag);
 
+
+/**
+ * @brief Clears the interrupt flags based on the provided initialization structure.
+ * @param usart_init_struct The USART initialization structure used to determine which flags to clear.
+ */
 void SetInterruptClearFlagRegister(UsartInitStruct &usart_init_struct);
 
+
+/**
+ * @brief Receives data from the USART peripheral.
+ * @param data_buffer The buffer where the received data will be stored.
+ */
 void ReceiveData(uintptr_t *data_buffer);
 
-void TransmitData(uintptr_t const *data);
+
+/**
+ * @brief Transmits a character via the USART peripheral.
+ * @param data The character to be transmitted.
+ */
+void TransmitData(char const *data);
 
 } // namespace USART
