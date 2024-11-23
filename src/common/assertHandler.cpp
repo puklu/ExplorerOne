32b60a3222cd3ae1ebc9a7a0c77dca45f0c4f3f5
puklu <vivekb126@gmail.com>
2024-11-23 10:45:00 +0000

    assert handler: add trace to assert handler

--- src/common/assertHandler.cpp
+++ src/common/assertHandler.cpp
@@ -1,9 +1,13 @@
 #include "assertHandler.hpp"
-
 #include "delay.hpp"
+#include "Trace.hpp"
 
-void assertHandler()
+
+void assertHandler(const char* file, int line)
 {
     // TODO: Improve me
+    
+    TRACE_LOG("Assertion failed in file %s at line %d", file, line);
+
     SOFTWARE_BREAKPOINT
 }
