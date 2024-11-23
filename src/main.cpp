356620ca28e0288a65364f51c11db526c491c5c4
puklu <vivekb126@gmail.com>
2024-11-23 10:25:47 +0000

    temp main code

--- src/main.cpp
+++ src/main.cpp
@@ -10,25 +10,17 @@
 #include "drivers/usart/UsartPin.hpp"
 #include "drivers/interfaces/PinFactory.hpp"
 
-#include "printf.h"
-
+#include "common/Trace.hpp"
 
 int main()
 {
 
     SystemInit();
 
-    UsartPinInitStruct pinInit = {};
-    pinInit.pin_name           = IO::ePin::IO_UART4_TX;
-    pinInit.alternate_function = IO::eAlternateFunction::IO_AF5;
-    pinInit.baud_rate          = USART::eBaudRate::USART_BAUD_RATE_115200;
-
-    [[maybe_unused]] PinBase const *usart_pin =
-        PinFactory::CreatePin(IO::ePinType::IO_PIN_TYPE_USART, pinInit);
-
     while (1)
     {
-        printf("duck %d\n", 2025);
+        TRACE_LOG("duck");
         delay(500000);
     }
-}
+
+}   
