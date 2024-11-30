77aa5032e814fdfede448234b9ece7f1f1ec3515
puklu <vivekb126@gmail.com>
2024-11-30 15:45:51 +0000

    asserthandler: add pin creation to asserthandler in case it is called before

--- src/common/assertHandler.cpp
+++ src/common/assertHandler.cpp
@@ -1,5 +1,6 @@
 #include "assertHandler.hpp"
 #include "drivers/interfaces/pinBank.hpp"
+#include "drivers/interfaces/PinFactory.hpp"
 #include "drivers/usart/UsartPin.hpp"
 #include "delay.hpp"
 #include "printf.h"
@@ -10,9 +11,19 @@
 
 void assertHandler(const char* file, int line)
 {
-    // TODO: Improve me
+    // If the usart print pin has not been created, create it
+    if(!activePrintUsartPin)
+    {
+        UsartPinInitStruct pinInit = {};
+        pinInit.pin_name           = IO::ePin::IO_UART4_TX_PRINT;
+        pinInit.alternate_function = IO::eAlternateFunction::IO_AF5;
+        pinInit.baud_rate          = USART::eBaudRate::USART_BAUD_RATE_115200;
 
-    // printf_set_putchar(UsartPutchar);
+        [[maybe_unused]] PinBase const *usart_print_pin =
+            PinFactory::CreatePin(IO::ePinType::IO_PIN_TYPE_PRINTING_USART, pinInit);
+        
+    }
+
     ActivateTraceForAssert();
     TRACE_LOG("Assertion failed in file %s at line %d", file, line);
     
