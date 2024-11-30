401810f07ec58e9067b3990d8cc57e1df8acb6ab
puklu <vivekb126@gmail.com>
2024-11-30 15:48:21 +0000

    temp main code

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 #include "common/Trace.hpp"
 #include "drivers/io/GpioPin.hpp"
 #include "drivers/leds/leds.hpp"
-#include "drivers/interfaces/IrqHandlers.cpp"
 #include "drivers/interfaces/pinBank.hpp"
 #include "drivers/interfaces/PinFactory.hpp"
 #include "drivers/usart/UsartPin.hpp"
@@ -17,15 +16,9 @@ int main()
 {
 
     SystemInit();
-
-    UsartPinInitStruct pinInit = {};
-    pinInit.pin_name           = IO::ePin::IO_UART4_TX_PRINT;
-    pinInit.alternate_function = IO::eAlternateFunction::IO_AF5;
-    pinInit.baud_rate          = USART::eBaudRate::USART_BAUD_RATE_115200;
-
-    [[maybe_unused]] PinBase const *usart_print_pin =
-        PinFactory::CreatePin(IO::ePinType::IO_PIN_TYPE_PRINTING_USART, pinInit);
     
+    ASSERT(isSystemInitialized);
+           
     int i = 0;
 
     while (1)
