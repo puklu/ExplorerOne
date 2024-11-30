ea407a98f04f2e44dfc9224423af4b9ae7ae61f8
puklu <vivekb126@gmail.com>
2024-11-30 15:47:01 +0000

    mcuInit: move inclusion of IrqHandler to SystemInit

--- src/drivers/mcu/mcuInit.cpp
+++ src/drivers/mcu/mcuInit.cpp
@@ -1,6 +1,10 @@
 #include "mcuInit.hpp"
 #include "stm32f303xc.h"
 
+#include "drivers/interfaces/IrqHandlers.cpp"
+
+bool isSystemInitialized = false;
+
 void SystemInit()
 {
     // TODO: Handle watchdog here
@@ -8,5 +12,7 @@ void SystemInit()
 
     // DBGMCU->APB1FZ |= DBGMCU_APB1_FZ_DBG_IWDG_STOP;  // Freeze IWDG in debug mode
     // DBGMCU->APB1FZ |= DBGMCU_APB1_FZ_DBG_WWDG_STOP;  // Freeze WWDG in debug mode
+
+    isSystemInitialized = true;
 
 }
