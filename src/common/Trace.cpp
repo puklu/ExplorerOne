1a6c03c16d7205ea5548a7e1d49efd0e89a61e2e
puklu <vivekb126@gmail.com>
2024-11-30 14:17:50 +0000

    trace: add pin creation to trace itself
    so that the user doesnt need to create a usart pin to use print

--- src/common/Trace.cpp
+++ src/common/Trace.cpp
@@ -11,7 +11,20 @@ bool Trace::isInitialized = false;
 
 void Trace::initialize()
 {
-    isInitialized = true;  
+    // If the usart print pin has not been created, create it
+    if(!activePrintUsartPin)
+    {
+        UsartPinInitStruct pinInit = {};
+        pinInit.pin_name           = IO::ePin::IO_UART4_TX_PRINT;
+        pinInit.alternate_function = IO::eAlternateFunction::IO_AF5;
+        pinInit.baud_rate          = USART::eBaudRate::USART_BAUD_RATE_115200;
+
+        [[maybe_unused]] PinBase const *usart_print_pin =
+            PinFactory::CreatePin(IO::ePinType::IO_PIN_TYPE_PRINTING_USART, pinInit);
+        
+    }
+    
+    isInitialized = true;
 }
 
 void Trace::print(const char* format, ...)
