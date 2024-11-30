c37c1f230466f1fd9b762070943ed8a428eded28
puklu <vivekb126@gmail.com>
2024-11-30 10:55:08 +0000

    pinFactory: add the ability to set callback functions for usart

--- src/drivers/interfaces/PinFactory.cpp
+++ src/drivers/interfaces/PinFactory.cpp
@@ -1,5 +1,6 @@
 #include "drivers/io/ExtiPin.hpp"
 #include "drivers/io/GpioPin.hpp"
+#include "drivers/usart/ServiceISRs.hpp"
 #include "drivers/usart/UsartPin.hpp"
 #include "pinBank.hpp"
 #include "PinFactory.hpp"
@@ -26,20 +27,48 @@ PinBase* PinFactory::CreatePin(IO::ePinType pin_type, const PinBaseInitStruct &p
             break;
         case IO::ePinType::IO_PIN_TYPE_EXTI:
             pin = new ExtiPin(static_cast<const ExtiPinInitStruct&>(pin_init_struct));
+            activeExtiPins[port_num][pin_num] = pin;
             break;
         case IO::ePinType::IO_PIN_TYPE_USART:{
             auto usart_pin = new UsartPin(static_cast<const UsartPinInitStruct&>(pin_init_struct));
-            pin = usart_pin;
 
-            activeUsartPin = usart_pin;
+            if(usart_pin->mpUsart == USART1)   
+            {
+                usart_pin->SetInterruptCallback(Usart1ServiceISR);
+            }
+            else if(usart_pin->mpUsart == USART2)
+            {
+                usart_pin->SetInterruptCallback(Usart2ServiceISR);
+            }
+            else if(usart_pin->mpUsart == USART3)
+            {
+                usart_pin->SetInterruptCallback(Usart3ServiceISR);
+            }
+            else if(usart_pin->mpUsart == UART4)
+            {
+                usart_pin->SetInterruptCallback(Uart4ServiceISR);
+            }
+            else if(usart_pin->mpUsart == UART5)
+            {
+                usart_pin->SetInterruptCallback(Uart5ServiceISR);
+            }
+
+            pin = usart_pin;
+            activeUsartPins[port_num][pin_num] = pin;
+            break;
+        } 
+        case IO::ePinType::IO_PIN_TYPE_PRINTING_USART:{
+            auto usart_pin = new UsartPin(static_cast<const UsartPinInitStruct&>(pin_init_struct));
+            usart_pin->SetInterruptCallback(UsartServiceISR_Printing);
+            pin = usart_pin;
+            activeUsartPins[port_num][pin_num] = usart_pin;
+            activePrintUsartPin = usart_pin;
 
             // Register the global UsartPutchar function
             printf_set_putchar(UsartPutchar);
-
-            // r?tPin::RegisterPrintf(usart_pin);
-
-            break;   
-        } 
+            
+            break;
+        }
         default:
             break;  // TODO: handle error
     }
