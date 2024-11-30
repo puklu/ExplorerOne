250bc651cbffccdd386ddaa1f951ab0b780bbe0b
puklu <vivekb126@gmail.com>
2024-11-30 10:56:56 +0000

    temp usart testing code

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,36 @@
 
 #include "common/assertHandler.hpp"
 #include "common/delay.hpp"
+#include "common/Trace.hpp"
 #include "drivers/io/GpioPin.hpp"
 #include "drivers/leds/leds.hpp"
-#include "drivers/io/IrqHandlers.cpp"
-#include "mcuInit.hpp"
-#include "drivers/usart/UsartPin.hpp"
+#include "drivers/interfaces/IrqHandlers.cpp"
+#include "drivers/interfaces/pinBank.hpp"
 #include "drivers/interfaces/PinFactory.hpp"
+#include "drivers/usart/UsartPin.hpp"
+#include "mcuInit.hpp"
 
-#include "common/Trace.hpp"
 
 int main()
 {
 
     SystemInit();
 
+    UsartPinInitStruct pinInit = {};
+    pinInit.pin_name           = IO::ePin::IO_UART4_TX_PRINT;
+    pinInit.alternate_function = IO::eAlternateFunction::IO_AF5;
+    pinInit.baud_rate          = USART::eBaudRate::USART_BAUD_RATE_115200;
+
+    [[maybe_unused]] PinBase const *usart_print_pin =
+        PinFactory::CreatePin(IO::ePinType::IO_PIN_TYPE_PRINTING_USART, pinInit);
+    
+    int i = 0;
+
     while (1)
     {
-        TRACE_LOG("duck");
-        delay(500000);
+        TRACE_LOG("duck %d", i);
+        i++;
+        delay(1000000);
     }
 
 }   
