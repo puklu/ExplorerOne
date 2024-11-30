81a834b457618866d993d02a3ecbbbe1a83986cf
puklu <vivekb126@gmail.com>
2024-11-30 10:48:16 +0000

    add trace printing for asserts

--- src/common/assertHandler.cpp
+++ src/common/assertHandler.cpp
@@ -1,13 +1,20 @@
 #include "assertHandler.hpp"
+#include "drivers/interfaces/pinBank.hpp"
+#include "drivers/usart/UsartPin.hpp"
 #include "delay.hpp"
+#include "printf.h"
+#include "printf_redirect.h"
 #include "Trace.hpp"
+
 
 
 void assertHandler(const char* file, int line)
 {
     // TODO: Improve me
-    
-    TRACE_LOG("Assertion failed in file %s at line %d", file, line);
 
+    // printf_set_putchar(UsartPutchar);
+    ActivateTraceForAssert();
+    TRACE_LOG("Assertion failed in file %s at line %d", file, line);
+    
     SOFTWARE_BREAKPOINT
 }
