f5a3b6fdf1a46d70e9357a0fe072b6f6a301bc66
puklu <vivekb126@gmail.com>
2025-01-08 21:39:57 +0000

    fill out alternate functions table for TIMs

--- src/common/AlternateFunctionsTable.hpp
+++ src/common/AlternateFunctionsTable.hpp
@@ -4,107 +4,107 @@
 #include "stm32f303xc.h"
 
 
-const void* aAltFunctionsAdressesPortA[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_FOR_PORT_A] = {
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2,
+inline const void* aAltFunctionsAdressesPortA[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_FOR_PORT_A] = {
+    {nullptr, TIM2, nullptr, nullptr, nullptr, nullptr, nullptr, USART2,
+    nullptr, TIM8, TIM8, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+
+    {nullptr, TIM2, nullptr, nullptr, nullptr, nullptr, nullptr, USART2,
+    nullptr, TIM15, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+
+    {nullptr, TIM2, nullptr, nullptr, nullptr, nullptr, nullptr, USART2,
+    nullptr, TIM15, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+
+    {nullptr, TIM2, nullptr, nullptr, nullptr, nullptr, nullptr, USART2,
+    nullptr, TIM15, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+
+    {nullptr, nullptr, TIM3, nullptr, nullptr, nullptr, nullptr, USART2,
     nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2,
+    {nullptr, TIM2, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
     nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2,
+    {nullptr, TIM16, TIM3, nullptr, TIM8, nullptr, TIM1, nullptr,
     nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2,
+    {nullptr, TIM17, TIM3, nullptr, TIM8, nullptr, TIM1, nullptr,
     nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, TIM1, USART1,
+    nullptr, nullptr, TIM4, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, TIM1, USART1,
+    nullptr, TIM15, TIM2, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+    {nullptr, TIM17, nullptr, nullptr, nullptr, nullptr, TIM1, USART1,
+    nullptr, nullptr, TIM2, TIM8, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, TIM1, USART1,
+    nullptr, nullptr, TIM4, TIM1, TIM1, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART1,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+    {nullptr, TIM16, nullptr, nullptr, nullptr, nullptr, TIM1, USART1,
+    nullptr, nullptr, TIM4, TIM1, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART1,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+    {nullptr, TIM16, nullptr, nullptr, nullptr, nullptr, nullptr, USART3,
+    nullptr, nullptr, TIM4, TIM1, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART1,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+    {nullptr, nullptr, nullptr, nullptr, nullptr, TIM8, TIM1, USART2,
+    nullptr, nullptr, TIM4, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART1,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
-
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART1,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
-
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART3,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
-
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
-
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+    {nullptr, TIM2, TIM8, nullptr, nullptr, nullptr, nullptr, USART2,
+    nullptr, TIM1, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 };
 
-const void* aAltFunctionsAdressesPortB[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_FOR_PORT_B] = {
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+inline const void* aAltFunctionsAdressesPortB[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_FOR_PORT_B] = {
+    {nullptr, nullptr, TIM3, nullptr, TIM8, nullptr, TIM1, nullptr,
+    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+
+    {nullptr, nullptr, TIM3, nullptr, TIM8, nullptr, TIM1, nullptr,
     nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 
     {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
     nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+    {nullptr, TIM2, TIM4, nullptr, TIM8, nullptr, nullptr, USART2,
+    nullptr, nullptr, TIM3, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+
+    {nullptr, TIM16, TIM3, nullptr, TIM8, nullptr, nullptr, USART2,
+    nullptr, nullptr, TIM17, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+
+    {nullptr, TIM16, TIM3, TIM8, nullptr, nullptr, nullptr, USART2,
+    nullptr, nullptr, TIM17, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+
+    {nullptr, TIM16, TIM4, nullptr, nullptr, nullptr, TIM8, USART1,
+    nullptr, nullptr, TIM8, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+
+    {nullptr, TIM17, TIM4, nullptr, nullptr, nullptr, nullptr, USART1,
+    nullptr, nullptr, TIM3, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+
+    {nullptr, TIM16, TIM4, nullptr, nullptr, nullptr, nullptr, nullptr,
+    nullptr, nullptr, TIM8, TIM1, nullptr, nullptr, nullptr, nullptr}, 
+
+    {nullptr, TIM17, TIM4, nullptr, nullptr, nullptr, nullptr, nullptr,
+    nullptr, nullptr, TIM8, nullptr, nullptr, nullptr, nullptr, nullptr}, 
+
+    {nullptr, TIM2, nullptr, nullptr, nullptr, nullptr, nullptr, USART3,
     nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2,
+    {nullptr, TIM2, nullptr, nullptr, nullptr, nullptr, nullptr, USART3,
     nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2,
+    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, TIM1, USART3,
     nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2,
+    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, TIM1, USART3,
     nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART1,
+    {nullptr, TIM15, nullptr, nullptr, nullptr, nullptr, TIM1, USART3,
     nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART1,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
-
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
-
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
-
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART3,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
-
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART3,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
-
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART3,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
-
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART3,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
-
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART3,
-    nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}, 
-
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr,
+    {nullptr, TIM15, TIM15, nullptr, TIM1, nullptr, nullptr, nullptr,
     nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
 };
 
-const void* aAltFunctionsAdressesPortC[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_FOR_PORT_C + 1] = {
+inline const void* aAltFunctionsAdressesPortC[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_FOR_PORT_C + 1] = {
     // The alternate functions in datasheet start from AF1 instead of
     // AF0. But an extra nullptr is added at index 0 of each row here
     // so that the code using this doesnt need to have a -1 
@@ -114,52 +114,52 @@ const void* aAltFunctionsAdressesPortC[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_
 
     {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, TIM1, nullptr},
 
     {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART1},
 
     {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART1},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM3, nullptr, TIM8, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM3, nullptr, TIM8, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM3, nullptr, TIM8, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM3, nullptr, TIM8, nullptr, TIM8, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, UART4, nullptr, USART3},
+    {nullptr, nullptr, nullptr, nullptr, TIM8, UART4, nullptr, USART3},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, UART4, nullptr, USART3},
+    {nullptr, nullptr, nullptr, nullptr, TIM8, UART4, nullptr, USART3},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, UART5, nullptr, USART3},
+    {nullptr, nullptr, nullptr, nullptr, TIM8, UART5, nullptr, USART3},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, nullptr, nullptr, TIM1, nullptr, nullptr, nullptr},
 
     {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
 
     {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
 };
 
-const void* aAltFunctionsAdressesPortD[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_FOR_PORT_D + 1] = {
+inline const void* aAltFunctionsAdressesPortD[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_FOR_PORT_D + 1] = {
     // The alternate functions in datasheet start from AF1 instead of
     // AF0. But an extra nullptr is added at index 0 of each row here
     // so that the code using this doesnt need to have a -1 
     {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, nullptr, nullptr, TIM8, nullptr, TIM8, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, UART5, nullptr, nullptr},
+    {nullptr, nullptr, TIM3, nullptr, TIM8, UART5, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2},
+    {nullptr, nullptr, TIM2, nullptr, nullptr, nullptr, nullptr, USART2},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2},
-
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2},
+    {nullptr, nullptr, TIM2, nullptr, nullptr, nullptr, nullptr, USART2},
 
     {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART2},
+    {nullptr, nullptr, TIM2, nullptr, nullptr, nullptr, nullptr, USART2},
+
+    {nullptr, nullptr, TIM2, nullptr, nullptr, nullptr, nullptr, USART2},
 
     {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART3},
 
@@ -169,57 +169,57 @@ const void* aAltFunctionsAdressesPortD[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_
 
     {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART3},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART3},
+    {nullptr, nullptr, TIM4, nullptr, nullptr, nullptr, nullptr, USART3},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM4, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM4, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM4, nullptr, nullptr, nullptr, nullptr, nullptr},
 };
 
-const void* aAltFunctionsAdressesPortE[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_FOR_PORT_E + 1] = {
+inline const void* aAltFunctionsAdressesPortE[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_FOR_PORT_E + 1] = {
     // The alternate functions in datasheet start from AF1 instead of
     // AF0. But an extra nullptr is added at index 0 of each row here
     // so that the code using this doesnt need to have a -1 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART1},
+    {nullptr, nullptr, TIM4, nullptr, TIM16, nullptr, nullptr, USART1},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART1},
+    {nullptr, nullptr, nullptr, nullptr, TIM17, nullptr, nullptr, USART1},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM3, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM3, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM3, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
-
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM3, nullptr, nullptr, nullptr, nullptr, nullptr},
 
     {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM1, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM1, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM1, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM1, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM1, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM1, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, TIM1, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, USART3},
+    {nullptr, nullptr, TIM1, nullptr, nullptr, nullptr, TIM1, nullptr},
+
+    {nullptr, nullptr, TIM1, nullptr, nullptr, nullptr, nullptr, USART3},
 };
 
-const void* aAltFunctionsAdressesPortF[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_FOR_PORT_F + 1] = {
+inline const void* aAltFunctionsAdressesPortF[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_FOR_PORT_F + 1] = {
     // The alternate functions in datasheet start from AF1 instead of
     // AF0. But an extra nullptr is added at index 0 of each row here
     // so that the code using this doesnt need to have a -1 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, TIM1, nullptr},
 
     {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
 
@@ -237,9 +237,9 @@ const void* aAltFunctionsAdressesPortF[IO_PIN_COUNT_PER_PORT][IO_ALT_FUNC_COUNT_
 
     {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, nullptr, TIM15, nullptr, nullptr, nullptr, nullptr},
 
-    {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
+    {nullptr, nullptr, nullptr, TIM15, nullptr, nullptr, nullptr, nullptr},
 
     {nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
 
