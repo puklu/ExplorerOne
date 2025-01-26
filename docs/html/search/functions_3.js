15a569399a4ffd021ddb48fd115a3370c520e940
puklu <vivekb126@gmail.com>
2025-01-26 21:10:59 +0000

    generate docs

--- docs/html/search/functions_3.js
+++ docs/html/search/functions_3.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['clearinterrupt_0',['clearinterrupt',['../classExtiPin.html#a58a869a8424ad2eba8eb5e5a8779ee50',1,'ExtiPin::ClearInterrupt()'],['../classBasicTimer.html#a22b18581f54a21163fa6da116719afe0',1,'BasicTimer::ClearInterrupt()'],['../classGeneralPurposeTimer.html#a42dcbf4465825491385d13756bb49a5e',1,'GeneralPurposeTimer::ClearInterrupt()']]],
   ['configurecapturecompareregisters_1',['ConfigureCaptureCompareRegisters',['../classGeneralPurposeTimer.html#a9275df8381f117ab950292b774b429b4',1,'GeneralPurposeTimer']]],
-  ['createmotordrivers_2',['CreateMotorDrivers',['../classDriveFactory.html#a8b06259ba98d5af4acb03fabd349b3b2',1,'DriveFactory']]]
+  ['createmdd3adrive_2',['CreateMdd3aDrive',['../classDriveFactory.html#a1c96ebb0579204c1082aeaa6d11d46b1',1,'DriveFactory']]]
 ];
