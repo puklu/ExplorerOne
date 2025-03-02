9cdcd42c03c6cee6452a6cc936934892b5e22594
puklu <vivekb126@gmail.com>
2025-03-02 14:35:21 +0000

    add doxygen comments

--- src/drivers/interfaces/ICriticalSectionGuard.hpp
+++ src/drivers/interfaces/ICriticalSectionGuard.hpp
@@ -1,9 +1,26 @@
 #pragma once
 
+
+/**
+ * @class ICriticalSectionGuard
+ * @brief Interface for managing critical sections.
+ *
+ * This interface defines methods for disabling and enabling interrupts,
+ * which are used to implement platform-specific critical section guards.
+ */
+
 class ICriticalSectionGuard
 {
 public:
     virtual ~ICriticalSectionGuard() = default;
+
+    /**
+     * @brief Disables interrupts to enter a critical section.
+     */
     virtual void DisableInterrupts() = 0;
+
+    /**
+     * @brief Enables interrupts to exit a critical section.
+     */
     virtual void EnableInterrupts() = 0;
 };
--- src/drivers/stm32f3discovery/common/Stm32f3CriticalSectionGuard.hpp
+++ src/drivers/stm32f3discovery/common/Stm32f3CriticalSectionGuard.hpp
@@ -3,6 +3,14 @@
 #include "stm32f303xc.h"
 #include "drivers/interfaces/ICriticalSectionGuard.hpp"
 
+
+/**
+ * @class Stm32f3CriticalSectionGuard
+ * @brief Platform-specific implementation of ICriticalSectionGuard for STM32F3.
+ *
+ * This class provides methods to disable and enable interrupts on the STM32F3
+ * microcontroller, ensuring atomic access to shared resources.
+ */
 class Stm32f3CriticalSectionGuard : public ICriticalSectionGuard
 {
 public:
