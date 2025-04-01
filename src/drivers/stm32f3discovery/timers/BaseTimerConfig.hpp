a276e17a83ba53123f1abdc17aaabbcc8b886972
puklu <vivekb126@gmail.com>
2025-04-01 19:25:06 +0000

    Timers: Move common code to BaseTimerConfig

--- src/drivers/stm32f3discovery/timers/BaseTimerConfig.hpp
+++ src/drivers/stm32f3discovery/timers/BaseTimerConfig.hpp
@@ -7,21 +7,65 @@
 #include "TimerChannel.hpp"
 
 using InterruptCallback = void(*)(void);
+
 class BaseTimerConfig
 {
 public:
 
     BaseTimerConfig() = default;
 
+    virtual ~BaseTimerConfig() = default;
+
+    /**
+     * @brief Prescaler value for the timer. Determines the clock division before counting.
+     */
     uint16_t mPrescalerValue = 7999;
+
+    /**
+     * @brief Auto-reload register value for the timer, used to set the period.
+     */
     uint16_t mAutoReloadRegisterValue = 1000;
+
+    /**
+     * @brief Optional callback function to be called on timer interrupt.
+     */
     InterruptCallback mCb = nullptr;
+
+    /**
+     * @brief One pulse mode configuration for the timer.
+     */
     Timer::eOnePulseMode mOnePulseMode = Timer::eOnePulseMode::DISABLE_ONE_PULSE_MODE;
+
+    /**
+     * @brief Auto-reload preload enable.
+     */
     Timer::eAutoReloadPreload mAutoReloadPreload = Timer::eAutoReloadPreload::ARR_BUFFERED;
+
+    /**
+     * @brief Update request source for the timer.
+     */
     Timer::eUpdateRequestSource mUpdateRequestSource = Timer::eUpdateRequestSource::ONLY_OVERFLOW_UNDERFLOW;
+
+    /**
+     * @brief Enable or disable event generation for timer updates.
+     */
     Timer::eUpdateEvent mEnableUpdateEvent = Timer::eUpdateEvent::ENABLE_EVENT_GENERATION;
+
+    /**
+     * @brief Master mode selection for timer synchronization.
+     */
     Timer::eMasterModeSelection mMasterModeSelection = Timer::eMasterModeSelection::SEND_RESET;
+
+    /**
+     * @brief Enable or disable DMA request on update.
+     */
     Timer::eUpdateDmaRequest mUpdateDmaRequest = Timer::eUpdateDmaRequest::DISABLE;
+
+    /**
+     * @brief Enable or disable update interrupt.
+     */
     Timer::eUpdateInterrupt mUpdateInterrupt = Timer::eUpdateInterrupt::ENABLE;
-    Milliseconds mTimerClockPeriodMs = Milliseconds{0};
+
+
+    Milliseconds mTimerClockPeriodMs = 1_ms;
 };
--- src/drivers/stm32f3discovery/timers/BasicTimerConfig.hpp
+++ src/drivers/stm32f3discovery/timers/BasicTimerConfig.hpp
@@ -1,32 +1,18 @@
 /**
- * @file GeneralPurposeTimerConfig.hpp
+ * @file BasicTimerConfig.hpp
  * 
- * @brief Header file containing configurations for general-purpose timers.
- *  This file defines class `GeneralPurposeTimerConfig`which serves as the overall configuration structure for a general-purpose
- *   timer. It holds arrays of `TimerChannel` instances and various timer settings, such as 
- *   clock division, direction, auto-reload, interrupt handling, DMA requests, and others.
+ * @brief Header file containing configurations for basic timers.
+ * The configurations is the same as BaseTimerConfig so it inherits all of them from the
+ * base class.
  * 
  * The configurations stored in these classes are used to setup timers.
  * 
- * These classes do **not** perform any actual timer operation; they serve as data containers 
- * to be used in conjunction with timer hardware setup functions.
- * 
- * @note Timer setup and operations are handled by other components or modules that utilize 
- *       these configuration classes.
  */
 
- #pragma once
+ #include "BaseTimerConfig.hpp"
 
- #include <array>
- #include <memory>
- #include <vector>
- #include "common/defines.hpp"
- #include "common/PinDefinitions.hpp"
- #include "TimerChannel.hpp"
- 
- using InterruptCallback = void(*)(void);
- 
- 
+ #pragma once
+  
 /**
  * @brief Class representing the initialization configuration for a basic timer.
  * 
@@ -34,21 +20,10 @@
  * a basic timer, including prescaler value, auto-reload register value, interrupt
  * callback, counter mode, preload settings, update sources, and more.
  */
-class BasicTimerConfig
+class BasicTimerConfig : public BaseTimerConfig
 {
 public:
  
    BasicTimerConfig() = default;
 
-   uint16_t mPrescalerValue = 7999;
-   uint16_t mAutoReloadRegisterValue = 1000;
-   InterruptCallback mCb = nullptr;
-   Timer::eOnePulseMode mOnePulseMode = Timer::eOnePulseMode::DISABLE_ONE_PULSE_MODE;
-   Timer::eAutoReloadPreload mAutoReloadPreload = Timer::eAutoReloadPreload::ARR_BUFFERED;
-   Timer::eUpdateRequestSource mUpdateRequestSource = Timer::eUpdateRequestSource::ONLY_OVERFLOW_UNDERFLOW;
-   Timer::eUpdateEvent mEnableUpdateEvent = Timer::eUpdateEvent::ENABLE_EVENT_GENERATION;
-   Timer::eMasterModeSelection mMasterModeSelection = Timer::eMasterModeSelection::SEND_RESET;
-   Timer::eUpdateDmaRequest mUpdateDmaRequest = Timer::eUpdateDmaRequest::DISABLE;
-   Timer::eUpdateInterrupt mUpdateInterrupt = Timer::eUpdateInterrupt::ENABLE;
-   Milliseconds mTimerClockPeriodMs = Milliseconds{1000};
 };
