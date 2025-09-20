4249d2fee6f9f4cd5d213200c3e088b7b9945718
puklu <vivekb126@gmail.com>
2025-09-20 18:49:40 +0000

    Documentation: Improve docs cmake and add a vscode task for doc generation for convenience.

--- cmake/docs.cmake
+++ cmake/docs.cmake
@@ -1,20 +1,26 @@
 find_program(DOXYGEN_EXECUTABLE NAMES doxygen) 
 
-if (DOXYGEN_EXECUTABLE)
-    set(DOXYFILE ${CMAKE_SOURCE_DIR}/docs/Doxyfile)
-
-    add_custom_target(doxygen
-    COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYFILE}
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMENT "Generating API documentation with Doxygen"
-    )
-
-    # Add another command to print a success message after the documentation is generated
-    add_custom_command(TARGET doxygen
-    POST_BUILD
-    COMMENT "Doxygen documentation generation complete."
-    )
-
-else()
-    message(STATUS "Doxygen not found, skipping documentation generation.")
+if(NOT DOXYGEN_EXECUTABLE)
+    message(FATAL_ERROR "Doxygen not found! Please install it.")
 endif()
+
+
+set(DOXYFILE ${CMAKE_SOURCE_DIR}/docs/Doxyfile)
+
+if (NOT EXISTS ${DOXYFILE})
+    message(FATAL_ERROR "Doxyfile not found at ${DOXYFILE}")
+endif()
+
+message("Doxyfile found at ${DOXYFILE}")
+
+add_custom_target(doxygen
+COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYFILE}
+WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+COMMENT "Generating API documentation with Doxygen"
+)
+
+# Add another command to print a success message after the documentation is generated
+add_custom_command(TARGET doxygen
+POST_BUILD
+COMMENT "Doxygen documentation generation complete."
+)
