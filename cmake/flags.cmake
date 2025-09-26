c502664f44ca2b26f927df22486f0862d730a39b
puklu <vivekb126@gmail.com>
2025-09-26 21:23:33 +0000

    libopencm3 integration: libopencm3 added to the repository but it is not being used at the moment. Conditionally being included based on USE_LIBOPENCM3 variable. Added for quick testing of something when needed.

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -6,10 +6,29 @@ set(CMAKE_OBJCOPY "arm-none-eabi-objcopy")
 
 # Set compiler flags
 set(CPU_FLAGS "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16")
-set(COMMON_FLAGS "-g3 -O0 -Wall -Werror -Wextra -Wshadow -fdata-sections -ffunction-sections")
-set(CMAKE_C_FLAGS "${CPU_FLAGS} ${COMMON_FLAGS}")
-set(CMAKE_CXX_FLAGS "${CPU_FLAGS} ${COMMON_FLAGS} -fno-exceptions")
-set(CMAKE_ASM_FLAGS "${CPU_FLAGS} ${COMMON_FLAGS}")
+
+set(COMMON_FLAGS
+    -g3 
+    -O0 
+    -Wall 
+    -Wextra 
+    -Wshadow 
+    -fdata-sections 
+    -ffunction-sections
+    )
+
+# libopencm3 causes many error because things gets redefined since they also exist
+# in CMSIS, so turn off Werror when libopencm3 is used
+if(NOT USE_LIBOPENCM3)
+    list(APPEND COMMON_FLAGS -Werror)
+endif()
+
+# convert the flags into a string for the following lines
+string(JOIN " " COMMON_FLAGS_STR ${COMMON_FLAGS})
+
+set(CMAKE_C_FLAGS "${CPU_FLAGS} ${COMMON_FLAGS_STR}")
+set(CMAKE_CXX_FLAGS "${CPU_FLAGS} ${COMMON_FLAGS_STR} -fno-exceptions")
+set(CMAKE_ASM_FLAGS "${CPU_FLAGS} ${COMMON_FLAGS_STR}")
 
 # Linker flags
 set(LINKER_SCRIPT "${CMAKE_SOURCE_DIR}/src/sys/stm32f3/STM32F303VCTX_FLASH.ld")
