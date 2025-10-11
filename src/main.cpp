65a6872d03861721bbbd1b21dd699f91d49d7cd1
puklu <vivekb126@gmail.com>
2025-10-11 19:25:38 +0000

    temp main.cpp code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 // src/main.cpp
+
 #include <climits>
 #include <cstdint>  // for uint8_t
 
@@ -35,6 +36,7 @@ int main()
 
     std::shared_ptr<PinBase> gpio_pin =
         PinFactory::CreatePin(IO::ePinType::IO_PIN_TYPE_GPIO, pinInit);
+    std::dynamic_pointer_cast<GpioPin>(gpio_pin)->Init(pinInit);
     auto pin = std::dynamic_pointer_cast<IDigitalPin>(gpio_pin);
 
     while (true)
