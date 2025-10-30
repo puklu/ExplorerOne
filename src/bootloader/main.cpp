4b4f0eae93adfff9a9338860d67424e63aba024c
puklu <vivekb126@gmail.com>
2025-10-30 12:52:17 +0000

    Restructure: Restrucure project for better modularity now that there is a separate bootloader logic.

--- src/bootloader/main.cpp
+++ src/bootloader/main.cpp
@@ -6,13 +6,13 @@ extern "C" void SystemInit()
 
 void JumpToApplication()
 {
-    using Function = void (*)();
-    uint32_t appStackAddress =
+    using app_entry_t = void (*)();
+    [[maybe_unused]] uint32_t appStackAddress =
         *reinterpret_cast<volatile uint32_t*>(APP_START_ADDRESS);
-    Function appResetHandler =
-        *reinterpret_cast<Function*>(APP_START_ADDRESS + 4);
+    app_entry_t appResetVector =
+        *reinterpret_cast<app_entry_t*>(APP_START_ADDRESS + 4);
 
-    appResetHandler();
+    appResetVector();
 }
 
 int main()
